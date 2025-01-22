89ef4dbf911ebd73ee7250fe21116f9960ebc73d
Ekramul Haque <ehr.ekramul@gmail.com>
2025-01-22 19:57:14 +0600

    Rapid Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'First App',
+      title: 'Test App',
       home: HomePage(),
     );
   }
@@ -20,131 +20,94 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  mySnackBar(message, context) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+  mySnackBar(message, context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message),duration: Duration(milliseconds: 500),),
+    );
   }
-
   @override
   Widget build(BuildContext context) {
+
+    var myItems = [
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Rabbil"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Rupom"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Hasan"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Salif"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Alhasan"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Nurza"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Nihan"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Nuri"
+      },
+      {
+        "img": "https://i.ytimg.com/vi/HADyEFViNxg/maxresdefault.jpg",
+        "title": "Sumaiya"
+      }
+    ];
+
+
     return Scaffold(
       appBar: AppBar(
+        title: Text('Test App'),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
-        title: Text('First App'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                mySnackBar('Appbar Search', context);
-              },
-              icon: Icon(Icons.search)),
-          IconButton(
-              onPressed: () {
-                mySnackBar('Appbar More', context);
-              },
-              icon: Icon(Icons.more_horiz)),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          mySnackBar('Floating Button', context);
-        },
-        shape: CircleBorder(),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        child: Icon(Icons.add),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.blue,
         currentIndex: 0,
         selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
             label: 'Home',
+            icon: Icon(Icons.home),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
             label: 'Settings',
-          ),
+            icon: Icon(Icons.settings),
+          )
         ],
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              mySnackBar('Home NavBar', context);
-              break;
-            case 1:
-              mySnackBar('Profile NavBar', context);
-              break;
-            case 2:
-              mySnackBar('Settings NavBar', context);
-              break;
-          }
+      ),
+      body: ListView.builder(
+        itemCount: myItems.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              mySnackBar('Card ${myItems[index]['title']}', context);
+            },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              width: double.infinity,
+              height: 200,
+              child: Image.network(
+                myItems[index]['img']!,
+                fit: BoxFit.fill,
+              ),
+            ),
+          );
         },
-      ),
-      drawer: SafeArea(
-        child: Drawer(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(topRight: Radius.circular(30)),
-          ),
-          child: ListView(
-            children: [
-              DrawerHeader(
-                padding: EdgeInsets.zero,
-                child: UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(color: Colors.blue),
-                  accountName: Text('First App'),
-                  accountEmail: Text('Testing App'),
-                  currentAccountPicture: Image.network(
-                    'https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png',
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('Profile'),
-                onTap: () {
-                  mySnackBar('Profile Drawer', context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-                onTap: () {
-                  mySnackBar('Settings Drawer', context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.security),
-                title: Text('Security'),
-                onTap: () {
-                  mySnackBar('Security Drawer', context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.contact_support_rounded),
-                title: Text('Support'),
-                onTap: () {
-                  mySnackBar('Support Drawer', context);
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(height: 50, width: 50, decoration: BoxDecoration(color: Colors.black)),
-          Container(height: 50, width: 50, decoration: BoxDecoration(color: Colors.black)),
-          Container(height: 50, width: 50, decoration: BoxDecoration(color: Colors.black)),
-          Container(height: 50, width: 50, decoration: BoxDecoration(color: Colors.black)),
-        ],
       ),
     );
   }
