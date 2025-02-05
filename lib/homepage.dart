8d8e305fd4f3a6de4b6e41e1b308abfa67b02de3
Ekramul Haque <ehr.ekramul@gmail.com>
2025-02-05 23:08:45 +0600

    Complete

---
+++ lib/homepage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Employee')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 15),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  label: Text('Name'), hintText: 'Enter your name'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  label: Text('Age'), hintText: 'Enter your age'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  label: Text('Salary'), hintText: 'Enter your salary'),
+            ),
+          ),
+          ElevatedButton(onPressed: (){}, child: Text('Add Employee'))
+        ],
+      ),
+    );
+  }
+}
