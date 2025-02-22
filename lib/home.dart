57a122288e6a44909562d584a178b5fac08262da
Ekramul Haque <ehr.ekramul@gmail.com>
2025-02-22 23:18:41 +0600

    Screen Dialogue Examples

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 12,
+            title: Text('Alert Dialogue'),
+            content: Text('Simple Alert Dialogue'),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Close'),
+              ),
+            ],
+          );
+        },
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +45,68 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    duration: Duration(seconds: 1),
+                    content: Text('SnackBar Pressed'),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () {
+                        print('Undo Pressed');
+                      },
+                    ),
+                  ),
+                );
+              },
+              child: Text('SnackBar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  backgroundColor: Colors.blue,
+                  context: context,
+                  builder: (context) {
+                    return Column(
+                      children: [Center(child: Text('This is bottom sheet'))],
+                    );
+                  },
+                );
+              },
+              child: Text('BottomSheet'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      elevation: 12,
+                      title: Text('Alert Dialogue'),
+                      content: Text('Simple Alert Dialogue'),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Close'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Alert Dialogue'),
+            ),
+          ],
+        ),
       ),
     );
   }
